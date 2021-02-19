bfd6eefa66998e373e9a9dfbb25c8e11410b6cc4
JEAN-BAPTISTE <jgeorges24@icloud.com>
2021-02-18 21:42:18 -0500

    Done.

---
+++ lib/student.rb
@@ -0,0 +1,19 @@
+# require'chatty_student.rb'
+
+
+class Student
+    attr_accessor :name 
+
+    def hello
+
+       @hello_phrase =  puts "Hey there! I'm so excited to learn stuff."
+    end
+
+
+
+    def raise_hand
+        @raise_hand = puts "Pick me!"
+    end
+
+
+end
